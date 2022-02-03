499173f05261ab2b2f9744def4895bbd612fd522
ashraful109 <ashrafulwev@gmail.com>
2022-02-03 21:53:08 +0600

    slider problem solv

--- home/models.py
+++ home/models.py
@@ -14,6 +14,10 @@ class House(models.Model):
     beds = models.CharField(max_length=20)
     baths = models.CharField(max_length=20)
     garage = models.CharField(max_length=20)
+    date_added = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
 
     def __str__(self):
         return self.name
--- home/views.py
+++ home/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
-from home.models import Slider
+from home.models import House,Slider
 
 # Create your views here.
 def home(request):
     slider = Slider.objects.all()
+    house = House.objects.all()
     context = {
-        'slider':slider
+        'slider':slider,
+        'house':house,
+        
+
     }
     return render(request, 'home/index.html',context)
